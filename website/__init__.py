9e389c193fc0a417934a46da890d759f65af039c
Sara Ismial <119016530+saraismial@users.noreply.github.com>
2024-09-21 23:39:46 -0500

    Update __init__.py
    The last code segment creates a database error. I have proposed the changes that fixes it according to the updated version of Flask.

--- website/__init__.py
+++ website/__init__.py
@@ -36,6 +36,7 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
+    if not path.exists('flaskwebsite/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
         print('Created Database!')
